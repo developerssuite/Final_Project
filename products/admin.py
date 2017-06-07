56e6c6c0980f19784e54c70c692b503ef1e4ff75
Zohaib Umar <Zohaib Umar>
2017-06-07 12:51:46 +0500

    Project Files deleted

--- products/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Product, NewsLetter, ContactUs, Categories
-
-admin.site.register(Product)
-admin.site.register(NewsLetter)
-admin.site.register(ContactUs)
-admin.site.register(Categories)
-# admin.site.register(User_Profile)
